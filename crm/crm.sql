3f9e7a5ca30966197835858b0f91b03f4ed170a4
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-04-13 02:07:33 +0000

    save during class 1

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,38 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+DROP TABLE IF EXISTS salesperson;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS activities;
+
+CREATE TABLE salesperson (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company_id INTEGER
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    notes TEXT,
+    occured_at TEXT,
+    salesperson_id INTEGER,
+    contact_id INTEGER
+);
